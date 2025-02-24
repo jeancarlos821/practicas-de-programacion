778d67e3a3102f2329c664eb1abe8828a5d4fa51
Jean <jc5137822@gmail.com>
2025-02-24 15:24:45 -0500

    operaciones aritméticas

---
+++ codigodepyton.py
@@ -0,0 +1,50 @@
+import math
+
+#operadores aritméticos
+
+print("Raíz cuadrada")
+
+print(math.sqrt(16))
+
+
+print("Suma de números enteros y decimales")
+
+print(3+2)
+
+print(float(1.0+2.1))
+
+
+print("Resta de números enteros y decimales")
+
+print(float(2.5-3.8))
+
+print(3-5)
+
+
+print("multiplicacion")
+
+print(3*6)
+
+
+print("exponente")
+
+print(2**6)
+
+print("División")
+
+print(3.5/2)
+
+print("Divición entera")
+
+print(3.5//2)
+
+print("Residuo")
+
+print(7%2)
+
+
+print("Definicion variables numéricas")
+
+a, b, c, d = 26, 11.3, 5, 3.5
+
+print(a+b)
